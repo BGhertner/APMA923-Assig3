a696990e3d0db02f1fe8ac7f7f6c97bd89c67d40
unknown <piyushbora1999@gmail.com>
2023-10-31 15:54:30 -0700

    Added Conjugate Descent

--- Code/CG.py
+++ Code/CG.py
@@ -1,17 +1,15 @@
 """
-BFGS - Benjamin Ghertner Oct 2023
+Conjugate Gradient - Piyush Agarwal Oct 2023
 """
 
 import numpy as np
 import scipy.linalg as sla
 
-
-def BFGS(x, f=None, g=None, H=None, hess=None, 
-         get_step=None, eps=1e-4, Xmax=10, 
-         kmax=100, verbose=False, gmethod=0,
-         Hmethod=0):
+def CG(x, f=None, g=None, get_step=None, eps=1e-4,
+        Xmax=10, kmax=100, verbose=False, gmethod=0):
+    
     """
-    BFGS - Benjamin Ghertner Oct 2023
+    CG - Piyush Agarwal Oct 2023
 
     Inputs:
 
@@ -23,15 +21,6 @@ def BFGS(x, f=None, g=None, H=None, hess=None,
     g: (optional) default=None, function handle for a function which takes an x position as an
         input and outputs a d X 1 vector cooresponding to the gradient of the objective function
         at the point x.
-
-    H: (optional) default=None, d X d numpy array, initial approximation for the inverse hessian 
-        approximation "H" this matrix needs to be symetric if it is not positive definite then a
-        mu will be found so that H + mu I > 0 and the need H = H + mu I will be used instead.
-
-    hess: (optional) default=None, d X d numpy array or function handle, hessian of the function 
-        at the initial point or function which takes a position vector x and outputs the hessian 
-        of the objective function at that point. This function or array will be used to calculate 
-        the initial H vector if the appropiate Hmethod choice is chosen (see Hmethod).
 
     get_step: (required) default=None, function handle which takes the below inputs and
                          returns the step-length for updating x
@@ -63,10 +52,6 @@ def BFGS(x, f=None, g=None, H=None, hess=None,
 
         0 - Use a supplied function for the gradient. g must be provided in this case.
 
-    Hmethod: (optional) default=1, integer - option for intial H calculation,
-
-        0 - Use a supplied initital H. H must be provided in this case.
-
     Returns:
 
     xk: d X 1 numpy vector - Final x point of the algorithm.
@@ -83,16 +68,8 @@ def BFGS(x, f=None, g=None, H=None, hess=None,
     #Start off x1 and x0 as the initial x point
     x1 = x0 = x.reshape(-1,1) #reshape to column vector if not already
     d = x0.shape[0] #columns of x are number of dimensions
-    #Start off g1 and g2 as the grad at the initial x point
+    #Start off g0 and g1 as the grad at the initial x point
     g1 = g0 = g(x0).reshape(-1,1)
-
-    #ID matrix for convience
-    I = np.eye(d,d)
-    
-    #Check that the initial H is SPD
-    eigvals = sla.eigvals(H)
-    #Fix if not...
-    if np.any(eigvals <= 0 ): H = H - np.min(eigvals)*1.1*I
 
     #initialize iteration count (k)
     k = 0
@@ -100,7 +77,9 @@ def BFGS(x, f=None, g=None, H=None, hess=None,
     #Initilization of step "previous" step sizes variables
     alpha0 = alpha1 = 1
 
-    #BFGS main loop
+    p = np.zeros(g1.shape())
+    beta = 0
+    #CG main loop
     #Stop if xk is outside Xmax distance from initial point
     #Stop if number of iterations (k) > max iterations (kmax)
     #Stop if ||grad(xk)|| or ||xk-1 - xk|| < stopping tolerance (eps)
@@ -109,14 +88,13 @@ def BFGS(x, f=None, g=None, H=None, hess=None,
            and np.min([sla.norm(g1,2), sla.norm(x0 - x1, 2)]) > eps) \
            or k < 2: #Run for at least two iterations.
         
-        #Numbered steps following algorithm 4.1 p.25 on notes
-        #Part 4 - Second order methods, Lecture 3 - Practical Implementation
-
+        #Numbered steps following CG Survey
+        
         #1
         x0 = x1
         g0 = g1
         #2
-        p = -H@g0
+        p = -1*g1 + beta*p
         #3
         alpha0 = alpha1
         alpha1 = get_step(f, x0, p, g0, alpha0, alpha1)
@@ -131,13 +109,29 @@ def BFGS(x, f=None, g=None, H=None, hess=None,
         #7
         y = g1 - g0
         #8
-        rho = 1/y.T@s
-        #9
-        H = (I - rho*s@y.T)@H@(I - rho*y@s.T) + rho*s@s.T
-        #10
+        beta = (sla.norm(g1, 2)**2)/(np.dot(p, y))
+        # 9
         k += 1
 
         if verbose: print(f'x = {x1}, g = {g1}')
 
 
     return x1, grads, obj_calls
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
