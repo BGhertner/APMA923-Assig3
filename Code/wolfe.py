8ebe9da8f859a937ae436b8e2fb2a95f994d52b5
Aspiire <121584814+Aspiire@users.noreply.github.com>
2023-11-03 13:53:53 -0700

    Add files via upload

--- Code/wolfe.py
+++ Code/wolfe.py
@@ -2,16 +2,17 @@
 Bisection for Wolfe conditions - Nick Huang, November 2023
 """
 
-import numpy as np
+import jax.numpy as np
 
-def wolfe_bisection(f, xk, pk, gk, mu, sigma, gmethod, alpha = 0., t = 1., beta = np.inf):
+def wolfe_bisection(f, g, xk, pk, gk, mu, sigma, alpha = 0., t = 1., beta = np.inf):
     """
     Args:
-        f: function handle to be minimized
+        f: function handle of function to be minimized
+        g: function handle of gradient of f
         xk: input vector at current step
         pk: chosen descent direction
         gk: gradient of f at xk
-        mu: hy.perparmeter with 0 < mu < sigma  < 1
+        mu: hyperparmeter with 0 < mu < sigma  < 1
         sigma: hyperparameter with 0 < mu < sigma < 1
         alpha: parameter to be used in wolfe, can be left untouched
         t: current proposed stepsize, can be left untouched
@@ -19,12 +20,60 @@ def wolfe_bisection(f, xk, pk, gk, mu, sigma, gmethod, alpha = 0., t = 1., beta 
     Returns: float t, the step length
 
     """
-    gxk1 = gmethod(f, xk + t * pk)
+    gxk1 = g(f, xk + t * pk)  # Create gradient of next vector
 
-    if f(xk + t * pk) > f(xk) + mu * t * np.dot(gk, pk):
-        wolfe_bisection(f, xk, pk, gk, mu, sigma, alpha, t=(alpha+t)/2, beta=t)
-    elif np.dot(gxk1, pk) < sigma * np.dot(gk, pk):
+    if f(xk + t * pk) > f(xk) + mu * t * np.dot(gk, pk):  # Check armijo
+        wolfe_bisection(f, xk, pk, gk, mu, sigma, alpha, t=(alpha+t)/2, beta=t)  # If yes, reset
+    elif np.dot(gxk1, pk) > sigma * np.dot(gk, pk):  # Wolfe
         if beta == np.inf:
+            # If infinite beta, reset t = 2 * alpha
             wolfe_bisection(f, xk, pk, gk, mu, sigma, gmethod, alpha=2*t, t=2*alpha, beta=beta)
+        else:
+            # Otherwise, reset t = (alpha + beta)/2
+            wolfe_bisection(f, xk, pk, gk, mu, sigma, gmethod, alpha=2 * t, t=(alpha + beta)/2, beta=beta)
     else:
-        return t
+        return t  # We have our step
+
+
+def soft_abs(x, eps=0.0001):
+    """
+    Numerically stable absolute value in terms of derivatives.
+
+    Args:
+        x: The point at which to evaluate the absolute value.
+        eps: Optional small positive float, smaller the better
+
+    Returns: float, soft abs of x
+
+    """
+    return np.sqrt(x**2 + eps**2)
+
+def strong_wolfe_bisection(f, xk, pk, gk, mu, sigma, gmethod, alpha = 0., t = 1., beta = np.inf):
+    """
+    Args:
+        f: function handle to be minimized
+        xk: input vector at current step
+        pk: chosen descent direction
+        gk: gradient of f at xk
+        mu: hyperparmeter with 0 < mu < sigma  < 1
+        gmethod: gradient method
+        sigma: hyperparameter with 0 < mu < sigma < 1
+        alpha: parameter to be used in wolfe, can be left untouched
+        t: current proposed stepsize, can be left untouched
+        beta: parameter to be used in wolfe, can be left untouched.
+    Returns: float, the step length
+
+    """
+    gxk1 = gmethod(f, xk + t * pk)  # Create gradient of next vector
+
+    if f(xk + t * pk) > f(xk) + mu * t * np.dot(gk, pk):  # Armjio
+        wolfe_bisection(f, xk, pk, gk, mu, sigma, alpha, t=(alpha+t)/2, beta=t)  # If yes, reset
+    elif soft_abs(np.dot(gxk1, pk)) > soft_abs(sigma * np.dot(gk, pk)):  # Uses soft abs to avoid numerical problems
+        if beta == np.inf:
+            # If infinite beta, reset t = 2 * alpha
+            wolfe_bisection(f, xk, pk, gk, mu, sigma, gmethod, alpha=2*t, t=2*alpha, beta=beta)
+        else:
+            # Otherwise, reset t = (alpha + beta)/2
+            wolfe_bisection(f, xk, pk, gk, mu, sigma, gmethod, alpha=2 * t, t=(alpha + beta)/2, beta=beta)
+    else:
+        return t  # We have our step
